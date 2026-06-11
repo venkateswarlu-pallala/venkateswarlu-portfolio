b4b10f04634d78d1dcf6ba8ac760b542cb0961e4
venkateswarlu-pallala <venki44456@gmailcom.com>
2026-06-11 19:34:06 +0530

    Configure production API URL

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const API_URL =
-  process.env.REACT_APP_API_URL  ;
+  process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
